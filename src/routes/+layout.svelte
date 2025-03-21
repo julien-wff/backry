057bdc29b39e7b6116605a041e85428e85d1dfa4
Julien W <cefadrom1@gmail.com>
2025-03-21 16:56:45 +0100

    chores: set Inter as the default font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
     import '../app.css';
+    import inter400 from '@fontsource/inter/files/inter-latin-400-normal.woff2';
+    import inter700 from '@fontsource/inter/files/inter-latin-700-normal.woff2';
 
     let { children } = $props();
 </script>
+
+<svelte:head>
+    <link as="font" href={inter400} rel="preload" type="font/woff2">
+    <link as="font" href={inter700} rel="preload" type="font/woff2">
+</svelte:head>
 
 {@render children()}
