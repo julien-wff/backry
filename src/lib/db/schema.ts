e6d6b6506d19331586ddc0359d9adc1fb85612c8
Julien W <cefadrom1@gmail.com>
2025-03-31 22:58:41 +0200

    feat: database table

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,6 +1,21 @@
-import { integer, sqliteTable } from 'drizzle-orm/sqlite-core';
+import { sql } from 'drizzle-orm';
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
-export const user = sqliteTable('user', {
-    id: integer('id').primaryKey(),
-    age: integer('age'),
+export const ELEMENT_STATUS = [ 'active', 'inactive', 'error' ] as const;
+export const DATABASE_ENGINES = [ 'postgresql-16' ] as const;
+
+export const databases = sqliteTable('databases', {
+    id: integer('id').primaryKey({ autoIncrement: true }),
+    name: text('name').notNull(),
+    engine: text('engine', { enum: DATABASE_ENGINES }).notNull(),
+    status: text('status', { enum: ELEMENT_STATUS }).notNull().default('active'),
+    error: text('error'),
+    created_at: text().default(sql`(CURRENT_TIMESTAMP)`),
+    updated_at: text().default(sql`(CURRENT_TIMESTAMP)`).$onUpdate(() => sql`(CURRENT_TIMESTAMP)`),
+    connection_string: text('connection_string'),
+    host: text('host'),
+    port: integer('port'),
+    database: text('database'),
+    username: text('username'),
+    password: text('password'),
 });
