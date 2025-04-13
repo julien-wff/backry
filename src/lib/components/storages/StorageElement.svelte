d6a478a531adc0b7932ae67fb3f9d2107d75caaf
Julien W <cefadrom1@gmail.com>
2025-04-13 17:30:50 +0200

    feat: display storages list

---
+++ src/lib/components/storages/StorageElement.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    import BaseListElement from '$lib/components/common/BaseListElement.svelte';
+    import { type storages } from '$lib/db/schema';
+    import Link from '@lucide/svelte/icons/link';
+    import Settings2 from '@lucide/svelte/icons/settings-2';
+
+    interface Props {
+        storage: typeof storages.$inferSelect;
+    }
+
+    let { storage }: Props = $props();
+    let status = $state(storage.status);
+</script>
+
+
+<BaseListElement editHref={`/storages/${storage.id}`}
+                 ondelete={() => console.log('Delete')}
+                 onduplicate={() => console.log('Duplicate')}
+                 onstatuschange={() => console.log('Status change')}
+                 status={status}
+                 title={storage.name}>
+    <div class="flex items-center gap-1">
+        <Settings2 class="w-4 h-4"/>
+        Type: {storage.type}
+    </div>
+
+    <div class="flex items-center gap-1">
+        <Link class="w-4 h-4"/>
+        <span>
+            URL:
+            <span class="font-mono">{storage.url}</span>
+        </span>
+    </div>
+</BaseListElement>
--- src/routes/(app)/storages/+page.svelte
+++ src/routes/(app)/storages/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import DatabaseElement from '$lib/components/databases/DatabaseElement.svelte';
+    import StorageElement from '$lib/components/storages/StorageElement.svelte';
     import TableHeader from '$lib/components/table/TableHeader.svelte';
     import CloudUpload from '@lucide/svelte/icons/cloud-upload';
     import type { PageData } from './$types';
@@ -11,3 +13,9 @@
 </script>
 
 <TableHeader icon={CloudUpload}>Storages</TableHeader>
+
+<div class="grid grid-cols-1 gap-4">
+    {#each data.storages as storage (storage.id)}
+        <StorageElement {storage}/>
+    {/each}
+</div>
