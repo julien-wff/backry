e3243f57540f8ff671f2b428d8411c33628badea
Julien W <cefadrom1@gmail.com>
2025-04-22 18:53:31 +0200

    feat: support image icons

--- src/lib/components/new-elements/NewElementCard.svelte
+++ src/lib/components/new-elements/NewElementCard.svelte
@@ -5,7 +5,7 @@
 
     interface Props {
         children: Snippet;
-        icon: typeof IconType;
+        icon: typeof IconType | string;
         href: string;
     }
 
@@ -15,7 +15,13 @@
 
 <a class="flex flex-col gap-4 items-center justify-center w-64 p-4 bg-base-100 rounded-box hover:scale-105 transition-all duration-200 ease-in-out"
    href="{page.url.pathname}/{href}">
-    <Icon class="w-12 h-12 text-primary"/>
+
+    {#if typeof Icon === 'string'}
+        <img src={Icon} alt="Icon" class="w-12 h-12"/>
+    {:else}
+        <Icon class="w-12 h-12 text-primary"/>
+    {/if}
+
     <h2 class="text-xl text-center font-bold flex items-center gap-2">
         {@render children()}
     </h2>
