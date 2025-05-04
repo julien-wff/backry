d0cd1510d052cfa816780f5ce78e232a81ac2480
Julien W <cefadrom1@gmail.com>
2025-05-04 19:00:05 +0200

    feat: revalidate page data every minute

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,9 +1,36 @@
 <script lang="ts">
+    import { invalidateAll } from '$app/navigation';
     import Navbar from '$lib/components/navbar/Navbar.svelte';
+    import { onMount } from 'svelte';
     import type { LayoutProps } from './$types';
 
     let { children, data }: LayoutProps = $props();
+
+    // Revalidate the whole page data every minute
+    // If a validation is skipped because the tab is in the background, it will revalidate when the tab is back in focus
+    let reloadSkipped = $state(false);
+
+    onMount(() => {
+        const reloadInterval = setInterval(() => {
+            if (!document.hidden) {
+                invalidateAll();
+            } else {
+                reloadSkipped = true;
+            }
+        }, 1000 * 60);
+
+        return () => clearInterval(reloadInterval);
+    });
+
+    function handleVisibilityChange() {
+        if (!document.hidden && reloadSkipped) {
+            reloadSkipped = false;
+            invalidateAll();
+        }
+    }
 </script>
+
+<svelte:document onvisibilitychange={handleVisibilityChange}/>
 
 <div class="flex min-h-screen bg-base-300">
     <Navbar errors={data.errors}/>
