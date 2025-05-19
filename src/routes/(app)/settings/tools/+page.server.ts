84a60e8d10c726b5ae94d0c4d0dabe5bc65e4cf5
Julien W <cefadrom1@gmail.com>
2025-05-19 16:05:43 +0200

    fix: type error

--- src/routes/(app)/settings/tools/+page.server.ts
+++ src/routes/(app)/settings/tools/+page.server.ts
@@ -14,13 +14,14 @@ export const load: PageServerLoad = async ({}) => {
         bun: {
             version: Bun.version_with_sha,
             error: null,
-            cmd: Bun.argv[0].split(path.sep).at(-1),
+            cmd: Bun.argv[0].split(path.sep).at(-1) ?? null,
             cmdResolved: Bun.argv.join(' '),
         },
         svelteKit: {
             version: svelteKitVersion,
             error: null,
             cmd: null,
+            cmdResolved: null,
         },
         restic: {
             version: resticVersion.isOk() ? resticVersion.value : null,
