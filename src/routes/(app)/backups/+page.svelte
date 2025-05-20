64346538c1d90fd86efbb64335c30c8f878c4f2d
Julien W <cefadrom1@gmail.com>
2025-05-20 10:33:27 +0200

    feat: fade transition

--- src/routes/(app)/backups/+page.svelte
+++ src/routes/(app)/backups/+page.svelte
@@ -11,6 +11,7 @@
     import relativeTime from 'dayjs/plugin/relativeTime';
     import utc from 'dayjs/plugin/utc';
     import { onMount } from 'svelte';
+    import { fade } from 'svelte/transition';
     import type { PageData } from './$types';
 
     dayjs.extend(relativeTime);
@@ -53,14 +54,16 @@
 <div class="grid grid-cols-1 gap-4">
     {#each runs as run (run.id)}
         {#if run.backups.length > 0}
-            <div class="grid grid-cols-1 gap-2 rounded-lg p-2 bg-base-200">
+            <div class="grid grid-cols-1 gap-2 rounded-lg p-2 bg-base-200" transition:fade={{ duration: 300 }}>
                 <div class="flex gap-2 text-sm align-center">
                     <RunOriginIndicator origin={run.origin}/>
                     Run #{run.id} - {run.backups[0].jobDatabase.job.name} - {dayjs.utc(run.createdAt).fromNow()}
                 </div>
 
                 {#each run.backups as backup (backup.id)}
-                    <BackupElement {backup}/>
+                    <div transition:fade={{ duration: 300 }}>
+                        <BackupElement {backup}/>
+                    </div>
                 {/each}
             </div>
         {/if}
