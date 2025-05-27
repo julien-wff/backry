c381526be114240bef917d05f894fffe05fdafcc
Julien W <cefadrom1@gmail.com>
2025-05-27 13:26:38 +0200

    feat: global error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { page } from '$app/state';
+    import Head from '$lib/components/common/Head.svelte';
+</script>
+
+<Head title={page.error?.message}/>
+
+<div class="flex w-full min-h-screen items-center justify-center p-4">
+    <div class="w-full max-w-md p-4 bg-base-100 rounded-box">
+        <h1 class="text-2xl font-bold">Oops!</h1>
+
+        <p class="mt-4 text-lg">
+            Something went wrong. Sorry for the inconvenience.
+        </p>
+
+        {#if page.error}
+            <p class="mt-2 opacity-50 grayscale">Error: {page.error.message}</p>
+        {/if}
+    </div>
+</div>
