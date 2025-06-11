1b104a1341136da6fe519c886cdab17e985febe4
Julien W <cefadrom1@gmail.com>
2025-06-11 17:45:31 +0200

    feat: allow to select unhealthy storages

--- src/routes/(app)/jobs/[id]/+page.svelte
+++ src/routes/(app)/jobs/[id]/+page.svelte
@@ -111,7 +111,7 @@
     <InputContainer for="backend" label="Storage backend">
         <select bind:value={storageBackend} class="w-full select" id="backend" required>
             {#each data.storages as storage (storage.id)}
-                <option value={storage.id} disabled={storage.status !== 'active'}>
+                <option value={storage.id} disabled={storage.status !== 'active' && storage.status !== 'unhealthy'}>
                     {storage.name}
                 </option>
             {/each}
