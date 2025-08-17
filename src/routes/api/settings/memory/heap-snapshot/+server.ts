22cf20d23e6a8a6240d7b6ee66d38ecb924187b0
Julien W <cefadrom1@gmail.com>
2025-08-17 15:33:10 +0200

    fix: memory leak

--- src/routes/api/settings/memory/heap-snapshot/+server.ts
+++ src/routes/api/settings/memory/heap-snapshot/+server.ts
@@ -1,25 +1,24 @@
 import type { RequestHandler } from './$types';
 import { getHeapSnapshot } from 'node:v8';
 import { logger } from '$lib/server/services/logger';
-import dayjs from 'dayjs';
-import { formatSize } from '$lib/helpers/format';
+import { Readable } from 'node:stream';
+import { formatDuration } from '$lib/helpers/format';
 
 export const GET: RequestHandler = async () => {
-    const start = dayjs();
+    const start = Date.now();
     logger.info(`Heap snapshot requested`);
 
     const snapshot = getHeapSnapshot(
         { exposeInternals: true, exposeNumericValues: true },
     );
-    const content = new Blob(await snapshot.toArray(), { type: 'application/octet-stream' });
 
-    logger.info(`Heap snapshot created in ${dayjs().diff(start, 'second', true).toFixed(2)} seconds, ${formatSize(content.size)}`);
+    logger.info(`Heap snapshot created in ${formatDuration((Date.now() - start) / 1000)}`);
 
-    return new Response(content, {
+    // @ts-expect-error ReadableStream of Response BodyInit is not the same as ReadableStream from Readable.toWeb
+    return new Response(Readable.toWeb(snapshot), {
         headers: {
             'Content-Type': 'application/octet-stream',
             'Content-Disposition': `attachment; filename="backry-heap-snapshot.heapsnapshot"`,
-            'Content-Length': content.size.toString(),
             'Cache-Control': 'no-cache, no-store, must-revalidate',
             'Pragma': 'no-cache',
         },
