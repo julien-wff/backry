a852ed47c945f1af79d7db4b09391a37f5bbedf5
Julien W <cefadrom1@gmail.com>
2025-08-26 18:10:56 +0200

    feat: feed the corrected connection string back to the field

--- src/routes/api/databases/check/+server.ts
+++ src/routes/api/databases/check/+server.ts
@@ -1,7 +1,7 @@
 import { apiError, apiSuccess } from '$lib/server/api/responses';
 import { ENGINES_METHODS } from '$lib/server/databases/engines-methods';
 import { parseRequestBody } from '$lib/server/schemas';
-import { databasesCheckRequest } from '$lib/server/schemas/api';
+import { type DatabaseCheckResponse, databasesCheckRequest } from '$lib/server/schemas/api';
 import type { RequestHandler } from './$types';
 
 export const POST: RequestHandler = async ({ request }) => {
@@ -17,5 +17,7 @@ export const POST: RequestHandler = async ({ request }) => {
         return apiError(res.error);
     }
 
-    return apiSuccess<{}>({});
+    return apiSuccess<DatabaseCheckResponse>({
+        connectionString: res.value,
+    });
 };
