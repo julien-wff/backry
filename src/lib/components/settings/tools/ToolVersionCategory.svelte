9f9f5a52751e49053f16fd0331ef2b9af211910a
Julien W <cefadrom1@gmail.com>
2025-09-19 14:24:41 +0200

    feat: group tools by category

---
+++ src/lib/components/settings/tools/ToolVersionCategory.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import type { Snippet } from 'svelte';
+
+    interface Props {
+        children: Snippet;
+        name: string;
+        icon: string;
+    }
+
+    let { children, name, icon }: Props = $props();
+</script>
+
+<div class="flex flex-col gap-2">
+    <h3 class="flex items-center gap-1 font-bold w-full border-b border-base-300">
+        <img alt="{name} icon" class="w-4 h-4" src={icon}/>
+        {name}
+    </h3>
+    {@render children()}
+</div>
