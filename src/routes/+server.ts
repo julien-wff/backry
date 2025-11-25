b2af838cddfde5bd447fa3f1761ad72b163a50da
Julien W <cefadrom1@gmail.com>
2025-11-25 18:35:31 +0100

    feat: redirect to setup when incomplete

--- src/routes/+server.ts
+++ src/routes/+server.ts
@@ -1,6 +1,14 @@
+import { SETUP_STEPS } from '$lib/common/constants';
+import { getSettings } from '$lib/server/settings/settings';
 import { redirect } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
-export const GET: RequestHandler = ({}) => {
-    return redirect(307, '/dashboard');
+export const GET: RequestHandler = async ({}) => {
+    const settings = await getSettings();
+    
+    if (settings.setupComplete) {
+        return redirect(307, '/dashboard');
+    } else {
+        return redirect(307, `/setup/${SETUP_STEPS[0]}`);
+    }
 };
