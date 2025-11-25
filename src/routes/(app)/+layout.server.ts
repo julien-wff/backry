8c5ac01f4d866cc905bf1db8c90af8635b97904e
Julien W <cefadrom1@gmail.com>
2025-11-25 17:20:02 +0100

    feat: redirect based on if setup is complete or not

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -1,12 +1,19 @@
 import { getErrorCountPerType, getWarningCountPerType } from '$lib/server/queries/shared';
+import { getSettings } from '$lib/server/settings/settings';
 import { areToolChecksSuccessful } from '$lib/server/shared/tool-checks';
+import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async () => {
-    const [ errors, warnings ] = await Promise.all([
+    const [ errors, warnings, settings ] = await Promise.all([
         getErrorCountPerType(),
         getWarningCountPerType(),
+        getSettings(),
     ]);
+
+    if (!settings.setupComplete) {
+        throw redirect(303, `/setup/${settings.setupCurrentStep}`);
+    }
 
     return {
         errors: {
