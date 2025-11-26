4ba9323f69bc5bc0f343f2c74d7f46dc12db5168
Julien W <cefadrom1@gmail.com>
2025-11-26 13:54:32 +0100

    feat: redirect to current step instead of first one

--- src/routes/setup/+page.server.ts
+++ src/routes/setup/+page.server.ts
@@ -1,7 +1,10 @@
 import { SETUP_STEPS } from '$lib/common/constants';
+import { getSettings } from '$lib/server/settings/settings';
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
-    throw redirect(303, `/setup/${SETUP_STEPS[0]}`);
+    const settings = await getSettings();
+
+    throw redirect(303, `/setup/${settings.setupCurrentStep ?? SETUP_STEPS[0]}`);
 };
